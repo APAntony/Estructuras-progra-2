27b0ee661bdd6b7cfad1e5a5c41ffa974ac76c27
Antony Artavia Palma <antonyartavia59@gmail.com>
2019-05-12 16:15:41 -0600

    feat: essqueleto(atributos) de Persona

--- Progra-2-Estructuras/estructuras.h
+++ Progra-2-Estructuras/estructuras.h
@@ -1,9 +1,20 @@
 #ifndef ESTRUCTURAS_H
 #define ESTRUCTURAS_H
 
+#include <QString>
 
 struct Persona{
-
+    int id;
+    QString nombre;
+    QString apellido;
+    QString pais;
+    QString creencia;
+    QString profesion;
+    QString correo;
+    //Falta:
+    //lo de la fecha de nacimiento
+    //Los arrays o listas de pecados y buenas acciones
+    //lista de hijos
 };
 
 #endif // ESTRUCTURAS_H
