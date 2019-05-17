9333bf38d01277f0946b0d27717baf49a74841d9
maurigamg <44620109+maurigamg@users.noreply.github.com>
2019-05-17 15:30:54 -0600

    Update estructuras.h

--- Progra-2-Estructuras/estructuras.h
+++ Progra-2-Estructuras/estructuras.h
@@ -8,6 +8,7 @@ struct Persona{
     QString nombre;
     QString apellido;
     QString pais;
+    QString continente
     QString creencia;
     QString profesion;
     QString correo;
@@ -15,6 +16,17 @@ struct Persona{
     //lo de la fecha de nacimiento
     //Los arrays o listas de pecados y buenas acciones
     //lista de hijos
+    Persona(int pId,QString pNom,QString pApell,QString continente,QString pPais,QString pCreencia,QString pProf,QString pCorreo){
+        id=pId;
+        nombre=pNom;
+        apellido=pApell;
+        pais=pPais;
+        creencia=pCreencia;
+        profesion=pProf;
+        correo=pCorreo;
+    }
+    Persona(){
+    }
 };
 
 #endif // ESTRUCTURAS_H
