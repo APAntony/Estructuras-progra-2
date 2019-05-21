a9fcf68a2c18265b3e33fe8d97a3c2ff97569e30
Antony Artavia Palma <antonyartavia59@gmail.com>
2019-05-20 19:25:20 -0600

    feat: AVL para humanos

--- Progra-2-Estructuras/estructuras.h
+++ Progra-2-Estructuras/estructuras.h
@@ -16,7 +16,7 @@ struct Persona{
     //lo de la fecha de nacimiento
     //Los arrays o listas de pecados y buenas acciones
     //lista de hijos
-    Persona(int pId,QString pNom,QString pApell,QString continente,QString pPais,QString pCreencia,QString pProf,QString pCorreo){
+    Persona(int pId,QString pNom,QString pApell,QString pContinente,QString pPais,QString pCreencia,QString pProf,QString pCorreo){
         id=pId;
         nombre=pNom;
         apellido=pApell;
@@ -24,6 +24,7 @@ struct Persona{
         creencia=pCreencia;
         profesion=pProf;
         correo=pCorreo;
+        continente = pContinente;
     }
 
     Persona(){
@@ -34,7 +35,7 @@ struct Persona{
 
 struct NodoPersona{
 	Persona persona;
-	NodoPersona siguiente;
+    NodoPersona *siguiente;
 	
 	NodoPersona(Persona pPersona){
 		persona=pPersona;
@@ -46,11 +47,11 @@ struct NodoPersona{
 };
 
 struct ListaPersonas{
-	NodoPersona primerNodo;
-	NodoPersona ultimoNodo;
+    NodoPersona *primerNodo;
+    NodoPersona *ultimoNodo;
 	
 	ListaPersonas(){
-		primerNodo=ultimoNodo=nullptr;
+        primerNodo = ultimoNodo = nullptr;
 	}
 	
 	void insertarPersona(Persona);
@@ -72,5 +73,91 @@ struct Mundo{
 };
 
 
+// Clase NodoAVL de Arbol AVL:
+struct NodoAVL {
+   Persona *persona;
+   int FE;
+   NodoAVL *padre;
+   NodoAVL *izquierdo;
+   NodoAVL *derecho;
+
+   friend struct AVL;
+
+    NodoAVL(Persona *per, NodoAVL *pad=nullptr, NodoAVL *izq=nullptr, NodoAVL *der=nullptr) :
+     persona(per), FE(0), padre(pad), izquierdo(izq), derecho(der) {
+
+    }
+};
+
+struct AVL {
+public: NodoAVL *raiz;
+  private:
+   enum {IZQUIERDO, DERECHO};
+   // Punteros de la lista, para cabeza y NodoAVL actual:
+
+   NodoAVL *actual;
+   int contador;
+   int altura;
+
+  public:
+   // Constructor y destructor básicos:
+   AVL() : raiz(nullptr), actual(nullptr) {}
+   ~AVL() { Podar(raiz); }
+
+   // Insertar en árbol ordenado:
+   void Insertar(Persona *per);
+
+   // Borrar un elemento del árbol:
+   void Borrar(int dat);
+
+   // Función de búsqueda:
+   bool Buscar(const int dat);
+
+   //Buscar un dato(NodoAVL) en especifica
+   NodoAVL* BuscarNodoAVL(const int dat);
+
+   // Comprobar si el árbol está vacío:
+   bool Vacio(NodoAVL *r) {
+        return r==nullptr;
+   }
+
+   // Comprobar si es un NodoAVL hoja:
+   bool EsHoja(NodoAVL *r) {
+        return !r->derecho && !r->izquierdo;
+   }
+
+   // Contar número de NodoAVLs:
+   int NumeroNodoAVLs();
+   int AlturaArbol();
+
+   // Calcular altura de un dato:
+   int Altura(const int dat);
+
+   // Devolver referencia al dato del NodoAVL actual:
+   int &ValorActual() {
+       return actual->persona->id;
+   }
+
+   // Moverse al NodoAVL raiz:
+   void Raiz() { actual = raiz; }
+
+   // Aplicar una función a cada elemento del árbol:
+   void InOrden(void (*func)(QString, int) , NodoAVL *NodoAVL=nullptr, bool r=true);
+   void PreOrden(void (*func)(QString, int) , NodoAVL *NodoAVL=nullptr, bool r=true);
+   void PostOrden(void (*func)(QString, int) , NodoAVL *NodoAVL=nullptr, bool r=true);
+
+  private:
+   // Funciones de equilibrado:
+   void Equilibrar(NodoAVL *NodoAVL, int, bool);
+   void RSI(NodoAVL* NodoAVL);
+   void RSD(NodoAVL* NodoAVL);
+   void RDI(NodoAVL* NodoAVL);
+   void RDD(NodoAVL* NodoAVL);
+
+   // Funciones auxiliares
+   void Podar(NodoAVL* &);
+   void auxContador(NodoAVL*);
+   void auxAltura(NodoAVL*, int);
+}; void Mostrar(QString n, int FE);
 
 #endif // ESTRUCTURAS_H
--- Progra-2-Estructuras/main.cpp
+++ Progra-2-Estructuras/main.cpp
@@ -2,17 +2,13 @@
 #include <QApplication>
 
 #include "estructuras.h"
-//#include <QtCore>
+#include <QtDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    Persona *p = new Persona();
-    QString lol;
-    p->obtenerHora(lol);
 
     return a.exec();
 }
